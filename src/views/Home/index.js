0c3315e6942effdd59ecf5cf1ed49e8daebc5ff7
Mister <muhammadkarim7000@gmail.com>
2022-06-23 11:43:40 +0500

    'category'

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from "./../../components/navbar"
 import Advertising from './../../components/Advertising'
 import Footer from './../../components/Footer'
+import Category from './../../components/category'
 
 function Home() {
   return (
     <div>
       <Navbar/>
       <Advertising />
+      <Category/>
       <Footer />
     </div>
   )
