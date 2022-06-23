3e57dd34c8d1b4a7bf688664b5900f87ae8829dd
Asliddinorip <asliddinorip31@gmail.com>
2022-06-23 08:43:28 +0500

    Advertising qo'shdim

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Navbar from "./../../components/navbar"
+import Advertising from './../../components/Advertising'
+import Footer from './../../components/Footer'
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Advertising />
+      <Footer />
     </div>
   )
 }
